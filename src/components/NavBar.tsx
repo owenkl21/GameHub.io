fd6481d589f8aba578dda7377bcee617f9805ca7
Owen Kleinhans <92853486+owenkl21@users.noreply.github.com>
2024-07-04 14:31:53 +0200

    I added Light and Dark toggle component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image, Text } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
+import ColorModeSwitch from './colorModeSwitch';
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack padding={'10px 10px 10px 0'} justifyContent={'space-between'}>
       <Image boxSize={'60px'} src={logo} />
       <Text> NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
